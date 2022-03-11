8bee1ca6fc698846c02bab89499355c13bd2d5d1
Dominik <hi@domthedev.com>
2022-03-11 09:16:17 +0100

    add dotenv

--- twitterClient.js
+++ twitterClient.js
@@ -1,8 +1,9 @@
 const {TwitterApi} = require("twitter-api-v2");
+require("dotenv").config();
 
 module.exports = new TwitterApi({
-    appKey: "BjW7k2ptvIohEUjLshvKfXQgQ",
-    appSecret: "PRG8mqzIvff0yF9Y6VMVDltpOeslE5Xj9DoILAZ0467TQIxdxp",
-    accessToken: "1500775661199400965-6EEDMMuWosNEQmjvTmpOG3ny3CX3DX",
-    accessSecret: "n8xgRrkKYqRZpiusgX92jL2DCgRnjQm2FiGtU16BunXik"
+    appKey: process.env.APP_KEY,
+    appSecret: process.env.APP_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_SECRET
 })
